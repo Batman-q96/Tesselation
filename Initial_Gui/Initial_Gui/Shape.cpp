0ea96e834b240c2db1543e898e1f6dcb2791db1f
Evan Feldman <ejosephfeldman@gmail.com>
2019-02-06 16:22:06 -0500

    Initial Commit

---
+++ Initial_Gui/Initial_Gui/Shape.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "Shape.h"
+
+
+Shape::Shape(int points)
+{
+	coord_list = 
+}
+
+
+Shape::~Shape()
+{
+	delete coord_list;
+}
---
+++ Initial_Gui/Initial_Gui/Shape.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <vector>
+class Shape
+{
+	std::vector<float> coord_list;
+public:
+	Shape();
+	~Shape();
+};
+
